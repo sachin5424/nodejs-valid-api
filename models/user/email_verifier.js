b9ea714caf05f531f695ff1cb93169fd4b93ae36
sachin5424 <sachinsagar516@gmail.com>
2021-01-31 21:47:00 +0530

    email opt modul

---
+++ models/user/email_verifier.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const Email_verification = mongoose.Schema({
+    email:{
+        type:String,
+        index:{
+            unique: true,
+        },
+        required:true,
+    },
+    email_verification_otp:{
+        type:Number,
+        unique:true,
+        required:true,
+        min:4,
+        max:4,
+    },
+
+})
+module.exports = mongoose.model('Email_verifications',Email_verification)
